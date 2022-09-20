83fbbf3ab968e46e4be345546b141b468de0ba8d
Austin <austinrdavies@outlook.com>
2022-09-20 08:50:09 -0700

    q3 project 0x02. C

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * islower - definition of the function.
+ * 
+ * Return - 1 if lowercase, 0 if non-lowercase eter
  */
 int _islower(int c)
 {
-	if (c >= 'a' || c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
-	{
-		return (0);
-	}
+	return (0);
 }
