837a789349e2bf376f2d099fe79c48e692aa9a7d
Austin <austinrdavies@outlook.com>
2022-09-20 09:55:48 -0700

    q6 project 0x02. C

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - defintion for the function
+ *
+ * @n: int for the number entered
+ *
+ * Return: 0
+ */
+int _abs(int)
+{
+	int n;
+
+	if (int >= 0)
+	{
+		_putchar(int);
+		return (0);
+	}
+	else if (int > 0)
+	{
+		n = int * -1; 
+		_putchar('+');
+		return (1);
+	}
+	return (1);
+}
