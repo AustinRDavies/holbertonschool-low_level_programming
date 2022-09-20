110483c86ce388386fe2f390813d4b6966906bb6
Austin <austinrdavies@outlook.com>
2022-09-20 09:39:06 -0700

    q5 project 0x02. C

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - defintion for the function
+ *
+ * @a - int for the number entered
+ *
+ * Return: 1 for greater and zero, 0 if zero, -1 if lest than zero
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return(1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return(-1);
+	}
+}
