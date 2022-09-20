fd756bf9935bbfc0688ad6e11e90bb805be538e8
Austin <austinrdavies@outlook.com>
2022-09-20 12:24:45 -0700

    q8 project 0x02. C

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+/**
+ * jack_bauer - will do minutes
+ */
+void jack_bauer(void)
+{
+	int f;
+	int s;
+	int t;
+	int l;
+	
+	f = 0
+	while (f <= '2')
+	{	
+		s = 0;
+		while (s <= '4')
+		{
+			t = 0;
+			while (t <= '5')
+			{
+				l = 0;
+				while (l <= '9')
+				{
+					_putchar(f);
+					_putchar(s);
+					_putchar(':');
+					_putchar(t);
+					_putchar(l);
+					l++;
+				}
+				t++;
+			}
+			s++;
+		}
+		f++;
+	}
+}
