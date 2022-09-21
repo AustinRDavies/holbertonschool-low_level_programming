c810ba4643da8657f33cba705371175a90abc6f2
Austin <austinrdavies@outlook.com>
2022-09-21 14:04:38 -0700

    q11 project 0x02. C

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_to_98 - will print number counting from n to 98
+ *
+ * @n: starting number
+ *
+ * Return - 0
+ */
+void print_to_98(int n)
+{
+	for(n <= '98'; n++)
+	{
+		_putchar(n);
+	}
+}
