903145fd50b46e3037706530f379dc8c876f7489
Austin <austinrdavies@outlook.com>
2022-09-22 09:13:03 -0700

    q5 project 0x04. C

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,27 @@
 #include "main.h"
 /**
  * more_numbers - will print 0-14
+ *
+ * Return: void
  */
 void more_numbers(void)
 {
 	int c;
 	int x;
+	int y;
+	int z;
 
 	for (c = '1'; c <= 10; x++)
 	{
 		for (x = '0'; x <= 14; x++)
 		{
+			y = x % 10;
+			z = x / 10;
 			if (x > 9)
 			{
-				_putchar((x / 10) + '0');
+				_putchar(z + '0');
 			}		
-			_putchar((x % 10) + '0');
+			_putchar(y + '0');
 		}
 		_putchar('\n');
 	}
