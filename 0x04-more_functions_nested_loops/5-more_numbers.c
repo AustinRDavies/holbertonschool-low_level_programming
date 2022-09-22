ce363c63f82a345d0dc869ee48455c5b8239b607
Austin <austinrdavies@outlook.com>
2022-09-22 08:46:05 -0700

    q5 project 0x04. C

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,12 +4,13 @@
  */
 void more_numbers(void)
 {
-	int x;
 	int c;
 
 	for (c = '0'; c <= 9; x++)
 	{
-		for (x = '0'; x <= 14; x++)
+		int x = '0';
+
+		for (x <= 14; x++)
 		{
 			if (x > 9)
 			{
