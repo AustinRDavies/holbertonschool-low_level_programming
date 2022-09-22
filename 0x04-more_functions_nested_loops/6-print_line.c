3fbc5648753c77b68ce74a0948cc84590207098e
Austin <austinrdavies@outlook.com>
2022-09-22 11:29:57 -0700

    q6 project 0x04. C

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - this will print an underscore n number of times
+ *
+ * @n: the number of times an underscore will be printed
+ *
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int x;
+	
+	if (n >= 1)
+	{
+		for (x = 0 ; x <= n; x++)
+		{
+			_putchar(x);
+		}
+	}
+	_putchar('\n');
+}
