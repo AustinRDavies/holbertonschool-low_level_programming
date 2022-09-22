b8d7a56171cdcd92d9fb1c76d2f1695666122a61
Austin <austinrdavies@outlook.com>
2022-09-22 12:18:01 -0700

    q7 project 0x04. c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal - this will print an underscore n number of times
+ *
+ * @n: length of line
+ *
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int x;
+	int y;
+
+	if (n >= 1)
+	{
+		for (x = 1; x <= n; x++)
+		{
+			if (x >= 0)
+			{
+				for (y = 1; y < x; y++)
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
