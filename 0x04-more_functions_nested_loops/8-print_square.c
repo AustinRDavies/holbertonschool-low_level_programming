2b755b8479505688a87c226e2df9cc16dbfc75ac
Austin <austinrdavies@outlook.com>
2022-09-22 12:33:55 -0700

    q8 project 0x04. C

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal - this will print an underscore n number of times
+ *
+ * @n: length of line
+ *
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size >= 1)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for(y = 1; y <= size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
