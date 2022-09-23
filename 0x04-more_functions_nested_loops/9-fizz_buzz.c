d6bbcad4b0d6ab26163b0598792122be47c5d24e
Austin <austinrdavies@outlook.com>
2022-09-23 07:16:19 -0700

    q9 project 0x04. C

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - fizzbuzz test program
+ *
+ * @i: counter
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	for (i = 1; i <= 100; i++)
+	{
+		if ((x % 15) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if ((x % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((x % 3) == 0);
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			putchar(i);
+		}
+	}
+	Return (0);
+}
