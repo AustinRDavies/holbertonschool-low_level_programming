8cc1833a9b888ee2f2f5a12d60f738dfebabd602
Austin <austinrdavies@outlook.com>
2022-09-23 11:33:35 -0700

    q0 project 0x05. C

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - this will take a pointer and update its value to 98
+ *
+ * Return: Void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Directory for project 0x05. C - Pointers, arrays and strings.
